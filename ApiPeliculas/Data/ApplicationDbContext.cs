63f8c323f86546f20e95cb13178d758d429b7277
Leonis Ocampo Nieves <leonis.ocampo@outlook.com>
2020-05-27 08:21:34 -0500

    Agregando cadena de conexión a la base de datos SqlServer

---
+++ ApiPeliculas/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ApiPeliculas.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPeliculas.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+        }
+
+        DbSet<Categoria> Categoria { get; set; }
+    }
+}
