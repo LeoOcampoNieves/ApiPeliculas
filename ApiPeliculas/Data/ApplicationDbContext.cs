2ded6ad8817b046ff17143800952806b09b9d4c0
Leonis Ocampo Nieves <leonis.ocampo@outlook.com>
2020-06-01 15:37:36 -0500

    Agregando el modelo Pelicula con sus Dto y migraciones

--- ApiPeliculas/Data/ApplicationDbContext.cs
+++ ApiPeliculas/Data/ApplicationDbContext.cs
@@ -14,5 +14,6 @@ namespace ApiPeliculas.Data
         }
 
         public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Pelicula> Pelicula { get; set; }
     }
 }
---
+++ ApiPeliculas/Models/Dtos/PeliculaCreateDto.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using static ApiPeliculas.Models.Pelicula;
+
+namespace ApiPeliculas.Models.Dtos
+{
+    public class PeliculaCreateDto
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "La ruta es obligatoria")]
+        public string RutaImagen { get; set; }
+        public IFormFile Foto { get; set; }
+        [Required(ErrorMessage = "La descripción es obligatorio")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "La duración es obligatoria")]
+        public string Duracion { get; set; }
+        public TipoClasificacion Clasificacion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+        // Creo la relación con la tabla categoría
+        public int CategoriaId { get; set; }
+    }
+}
